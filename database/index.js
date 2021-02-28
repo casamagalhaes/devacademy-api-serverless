9db4f5a55b95fea95a4079ade988e595a0935aa8
Anderson Andrade <anderson.asa89@gmail.com>
2021-02-28 19:27:50 -0300

    update sqlite

--- database/index.js
+++ database/index.js
@@ -1,63 +1,71 @@
-const fs = require('fs');
-const sqlite3 = require('sqlite3').verbose();
-const filename = process.env.DATABASE || ':memory:';
+const fs = require("fs");
+const os = require("os");
+const sqlite3 = require("sqlite3").verbose();
+const executionEnv = process.env.AWS_EXECUTION_ENV || "local";
+const isLambda = executionEnv.startsWith("AWS");
+const filename = process.env.DATABASE || `${os.tmpdir()}/db.sqlite`;
 
 const open = () => {
-    return new sqlite3.Database(filename);
-}
+  return new sqlite3.Database(filename);
+};
 
-const create = async () => {
-    const db = await open();
-    const script = fs.readFileSync(`${__dirname}/migrations/database.sql`).toString();
-    const statements = script.split('GO;');
-    db.serialize(() => {
-        for (statement of statements) {
-            db.run(statement);
-        }
-    });
-    await db.close();
-    console.log('database created');
-}
+const create = async (directory = __dirname) => {
+  const db = await open();
+  const script = fs.readFileSync(`${directory}/migrations/database.sql`).toString();
+  const statements = script.split("GO;");
+  db.serialize(() => {
+    for (statement of statements) {
+      db.run(statement);
+    }
+  });
+  await db.close();
+  console.log("database created");
+};
 
 const destroy = async () => {
-    if (filename !== ':memory:') {
-        if (fs.existsSync(filename)) {
-            fs.unlinkSync(filename);
-        }
+  if (filename !== ":memory:") {
+    if (fs.existsSync(filename)) {
+      fs.unlinkSync(filename);
     }
-    console.log('database destroyed');
-}
+  }
+  console.log("database destroyed");
+};
 
 const reset = async () => {
-    await destroy();
-    await create();
-}
+  await destroy();
+  await create();
+};
 
 const execute = async (sql, params, connection) => {
-    const db = connection || await open();
-    return new Promise((resolve, reject) => {
-        db.run(sql, params, function (err) {
-            if (err) reject(err)
-            else resolve(this.changes);
-        })
+  const db = connection || (await open());
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) reject(err);
+      else resolve(this.changes);
     });
-}
+  });
+};
 
 const query = async (sql, params, connection) => {
-    const db = connection || await open();
-    return new Promise((resolve, reject) => {
-        db.all(sql, params, (err, rows) => {
-            if (err) reject(err);
-            else resolve(rows);
-        });
+  const db = connection || (await open());
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
     });
-}
+  });
+};
+
+const bootstrap = async () => {
+  await create();
+};
 
 module.exports = {
-    open,
-    execute,
-    query,
-    create,
-    destroy,
-    reset
-}
+  open,
+  execute,
+  query,
+  create,
+  destroy,
+  reset,
+  bootstrap,
+};
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const { bootstrap } = require("./database");
 const App = require("./lib/app");
 const productsRouter = require("./routes/products");
 
 module.exports.handler = async (event) => {
   const app = new App(event);
-
   try {
+    await bootstrap();
     app.router(productsRouter);
     return await app.handler();
   } catch (error) {
